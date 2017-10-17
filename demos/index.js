9cdf0a8c4e43f904fea57e52cca70bdd5f2dd2cc
Fabien JUIF <fabien.juif@zenika.com>
2017-10-17 20:35:55 +0200

    :ok_hand: @delphinemillet review

--- demos/index.js
+++ demos/index.js
@@ -6,9 +6,9 @@ const Powerpuff = types
     mood: types.enumeration(['happy', 'aggressive']),
   })
 
-const rebelle = Powerpuff.create({ name: 'Rebelle', mood: 'aggressive' })
+const rebelle = Powerpuff.create({ name: 'Rebelle', mood: 'happy' })
 
 // /!\ doesn't work since we don't use an action to mutate the model
-// rebelle.mood = 'happy'
+// rebelle.mood = 'aggressive'
 
 console.log(JSON.stringify(rebelle.toJSON(), null, 2))
